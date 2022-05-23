29ecf4361496149f0303b4b85c69bbec7be61f81
AnOwlbear <anowlbear@protonmail.com>
2022-05-23 23:59:01 +0100

    Added ability to logout

---
+++ SimpleForum/Pages/Logout.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleForum.Pages;
+
+[Authorize]
+public class Logout : PageModel
+{
+    public async Task<IActionResult> OnPost()
+    {
+        await HttpContext.SignOutAsync();
+        return RedirectToPage("Index");
+    }
+}
