7deffa30c234ca82e494c2e3cccdcf6808ea2c8e
AnOwlbear <anowlbear@protonmail.com>
2022-05-25 03:14:53 +0100

    Added profile dropdown

--- SimpleForum/wwwroot/js/site.js
+++ SimpleForum/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿const toggleDropdown = () => {
+    const dropdown = document.querySelector('.profile-dropdown');
+    if (dropdown.classList.contains('hidden')) dropdown.classList.remove('hidden');
+    else dropdown.classList.add('hidden');
+}
