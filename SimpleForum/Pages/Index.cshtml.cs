adc0abee8990b6268f2e44a88f60a6feefdc82c8
AnOwlbear <anowlbear@protonmail.com>
2022-06-29 00:07:21 +0100

    Added WIP forum list page

--- SimpleForum/Pages/Index.cshtml.cs
+++ SimpleForum/Pages/Index.cshtml.cs
@@ -1,18 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SimpleForum.Data;
+using SimpleForum.Models;
 
 namespace SimpleForum.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly IMediator _mediator;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public ForumsResponse Data { get; set; }
+
+    public IndexModel(IMediator mediator) => _mediator = mediator;
+
+    public async Task OnGet()
     {
-        _logger = logger;
+        Data = await _mediator.Send(new ForumsRequest());
     }
+    
+    public record ForumsRequest : IRequest<ForumsResponse>;
 
-    public void OnGet()
+    public record ForumsResponse(IEnumerable<Forum> Forums);
+
+    public class Handler : IRequestHandler<ForumsRequest, ForumsResponse>
     {
+        private readonly SimpleForumContext _context;
 
+        public Handler(SimpleForumContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ForumsResponse> Handle(ForumsRequest request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Forum> forums = await _context.Forums.ToListAsync(cancellationToken);
+            return new ForumsResponse(forums);
+        }
     }
 }
