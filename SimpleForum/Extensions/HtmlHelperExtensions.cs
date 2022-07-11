f221ca41ff9dddbbd94aee9b3c7f431acfa47be4
AnOwlbear <anowlbear@protonmail.com>
2022-07-12 00:15:08 +0100

    Created extension method for generating profile image urls

---
+++ SimpleForum/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace SimpleForum.Extensions;
+
+public static class HtmlHelperExtensions
+{
+    public static string ProfileImgUrl(this IHtmlHelper htmlHelper, string filename) =>
+        $"/images/profile_images/{filename}.jpeg";
+}
