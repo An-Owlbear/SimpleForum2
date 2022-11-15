1ab8038412d3c9a0cf7299eeafc6e63ca51b2833
AnOwlbear <anowlbear@protonmail.com>
2022-11-15 16:15:35 +0000

    Added user bios to model

---
+++ SimpleForum/Migrations/20221115161455_Added_user_bios.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleForum.Migrations
+{
+    public partial class Added_user_bios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "User");
+        }
+    }
+}
